14a709b53a70a614d2a5411fbef9e59126b0fc76
Denis Protassoff <denis.protassoff@scandiweb.com>
2022-12-20 14:41:51 +0300

    Added Slider and Slide repositories

---
+++ Model/SlideRepository.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Scandiweb\Slider\Model;
+
+use Magento\Framework\Exception\CouldNotDeleteException;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Scandiweb\Slider\Api\SlideRepositoryInterface;
+use Scandiweb\Slider\Api\Data\SlideInterface;
+use Scandiweb\Slider\Model\ResourceModel\Slide as SlideResource;
+use Scandiweb\Slider\Model\ResourceModel\Slide\CollectionFactory as SlideCollectionFactory;
+
+/**
+ * Class SlideRepository
+ */
+class SlideRepository implements SlideRepositoryInterface
+{
+
+    /**
+     * @var SlideFactory
+     */
+    protected $slideFactory;
+
+    /**
+     * @var SlideResource
+     */
+    protected $slideResource;
+
+    /**
+     * @var SlideCollectionFactory
+     */
+    protected $slideCollectionFactory;
+
+    public function __construct(
+        SlideFactory $slideFactory,
+        SlideResource $slideResource,
+        SlideCollectionFactory $slideCollectionFactory
+    ) {
+        $this->slideFactory = $slideFactory;
+        $this->slideResource = $slideResource;
+        $this->slideCollectionFactory = $slideCollectionFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function get($id)
+    {
+        $slide = $this->slideFactory->create();
+        $this->slideResource->load($slide, $id);
+
+        if (!$slide->getId()) {
+            throw new NoSuchEntityException(__('Slide with ID "%1" doesn\'t exist', $id));
+        }
+
+        return $slide;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function save(SlideInterface $slide)
+    {
+        $this->slideResource->save($slide);
+
+        return $slide;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function delete(SlideInterface $slide)
+    {
+        try {
+            $this->slideResource->delete($slide);
+        } catch (\Exception $exception) {
+            throw new CouldNotDeleteException(
+                __('Could not delete slide: %1', $exception->getMessage())
+            );
+        }
+
+        return true;
+    }
+}
---
+++ Model/SliderRepository.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Scandiweb\Slider\Model;
+
+use Magento\Framework\Exception\CouldNotDeleteException;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Scandiweb\Slider\Api\SliderRepositoryInterface;
+use Scandiweb\Slider\Api\Data\SliderInterface;
+use Scandiweb\Slider\Model\ResourceModel\Slider as SliderResource;
+use Scandiweb\Slider\Model\ResourceModel\Slider\CollectionFactory as SliderCollectionFactory;
+
+/**
+ * Class SliderRepository
+ */
+class SliderRepository implements SliderRepositoryInterface
+{
+
+    /**
+     * @var SliderFactory
+     */
+    protected $sliderFactory;
+
+    /**
+     * @var SliderResource
+     */
+    protected $sliderResource;
+
+    /**
+     * @var SliderCollectionFactory
+     */
+    protected $sliderCollectionFactory;
+
+    public function __construct(
+        SliderFactory $sliderFactory,
+        SliderResource $sliderResource,
+        SliderCollectionFactory $sliderCollectionFactory
+    ) {
+        $this->sliderFactory = $sliderFactory;
+        $this->sliderResource = $sliderResource;
+        $this->sliderCollectionFactory = $sliderCollectionFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function get($id)
+    {
+        $slider = $this->sliderFactory->create();
+        $this->sliderResource->load($slider, $id);
+
+        if (!$slider->getId()) {
+            throw new NoSuchEntityException(__('Slider with ID "%1" doesn\'t exist', $id));
+        }
+
+        return $slider;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function save(SliderInterface $slider)
+    {
+        $this->sliderResource->save($slider);
+
+        return $slider;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function delete(SliderInterface $slider)
+    {
+        try {
+            $this->sliderResource->delete($slider);
+        } catch (\Exception $exception) {
+            throw new CouldNotDeleteException(
+                __('Could not delete slider: %1', $exception->getMessage())
+            );
+        }
+
+        return true;
+    }
+}
